3ffaf5f1432e28995dbe43be6578b7c5bfa00627
amr khafagy <amr.khafagy@yahoo.com>
2021-06-08 17:35:45 +0100

    add

--- APP/js/custom.js
+++ APP/js/custom.js
@@ -4,9 +4,30 @@
   
       // PRE LOADER
       $(window).load(function(){
-        $('.preloader').fadeOut(1000); // set duration in brackets    
+          $('.preloader').fadeOut(1000);
+        // set duration in brackets
       });
   //Navigation Section
   $('.navbar-collapse a').on('click',function(){
     $(".navbar-collapse").collapse('hide');
   });
+  $('.owl-carousel').owlCarousel({
+    animateOut: 'fadeOut',
+    items:1,
+    loop:true,
+    autoplay:true,
+  })
+    $.stellar();
+    $(function() {
+        $('.navbar-default a, #home a, footer a').on('click', function(event) {
+          var $anchor = $(this);
+            $('html, body').stop().animate({
+              scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+              event.preventDefault();
+        });
+      });  
+      new WOW({ mobile: false }).init();
+
+    })(jQuery);
+    
