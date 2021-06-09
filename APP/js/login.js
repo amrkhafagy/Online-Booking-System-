0ff0bc733543253bd175df9bd15e69bd1da6e4b1
amr khafagy <amr.khafagy@yahoo.com>
2021-06-09 18:12:18 +0100

    js 2

--- APP/js/login.js
+++ APP/js/login.js
@@ -13,3 +13,20 @@ $( document ).ready(function() {
           email : $("#userName").val(),
           password :  $("#password").val()
         }
+        console.log("post data",formData);
+        // DO POST
+        $.ajax({
+        type : "POST",
+        contentType : "application/json",
+        url : 'http://localhost:3000/Api/v1/user/signin',
+        data : JSON.stringify(formData),
+        dataType : 'json',
+        success : function(customer) {
+          // alert("Login Success");
+          console.log("dsfsfsdfsfsd",customer);
+          localStorage.setItem("email", customer.data.email);
+          localStorage.setItem("userid", customer.data._id);
+          localStorage.setItem("username", customer.data.name);
+
+          window.location.replace('index.html');
+        },
