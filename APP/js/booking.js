e62aea247ee4623093fbc8b73bf3e3bb565c603f
amr khafagy <amr.khafagy@yahoo.com>
2021-06-09 18:06:29 +0100

    init

--- APP/js/booking.js
+++ APP/js/booking.js
@@ -35,29 +35,27 @@ $(document).ready(function () {
     }
 })
 
-            function ajaxPost() {
-                userid
-                var id = localStorage.getItem("userid");
+var todaydate = new Date();
+var day = String(todaydate.getDate()).padStart(2, '0');
+var month = String(todaydate.getMonth() + 1).padStart(2, '0');
+var year = todaydate.getFullYear();
+todaydate = year + '-' + month + '-' + day;
+document.getElementById("date").min=todaydate;
 
-                var formData = {
-                    date: $("#date").val(),
-                    time: $("#time").val(),
-                    doctor: $("#doctor").val(),
-                    comment: $("#message").val(),
-                    user: id,
-                    status: "pending"
-
-                }
-
-
-                console.log("post data", formData);
-         
-
-            }
-            function resetData() {
-                $("#name").val("");
-                $("#email").val("");
-            }
-        });
-    }
-});
+function logout() {
+     localStorage.clear();
+     window.location.replace('login.html');
+}
+$(document).ready(function () {
+     var userid = localStorage.getItem("userid");
+     //alert("Hello "+localStorage.getItem("username"))
+     var user_name= "Hello "+localStorage.getItem("username");
+     document.getElementById("hello").innerHTML=user_name;
+     if (!userid) {
+          alert("Please Login to Access..??");
+          window.location.replace('login.html');
+     }
+     $("#appointment-form").submit(function (event) {
+          event.preventDefault();
+          ajaxPost();
+     });
